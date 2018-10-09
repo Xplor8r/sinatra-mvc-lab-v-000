cc6499f81402caee45a9f58052e3487a35a6aadc
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-09 18:35:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @user_input = PigLatinizer.new(params[:user_text])
+    @user_input = PigLatinizer.new(params[:user_phrase])
     erb :results
   end
 end
