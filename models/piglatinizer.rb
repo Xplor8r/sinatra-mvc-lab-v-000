4c965f5873b11ede138c96651c8379439e03d548
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-09 18:55:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,53 +1,32 @@
 class PigLatinizer
-attr_reader :text
 
-  def initialize(text)
-    @text = text
+  def piglatinize(input_str)
+    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    puts x
+    x
+  end
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
   end
 
-  def translate
+  def piglatinize_word(word)
+    if !consonant?(word[0])
+      word = word + "w"
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
 
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    words = text.split(' ')
-    result = []
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
 
-    words.each_with_index do |word, i|
-      translation = ''
-      qu = false
-      if vowels.include? word[0]
-        translation = word + 'ay'
-        result.push(translation)
-      else
-        word = word.split('')
-        count = 0
-        word.each_with_index do |char, index|
-          if vowels.include? char
-              if char == 'u' and translation[-1] == 'q'
-                qu = true
-                translation = words[i][count + 1..words[i].length] + translation + 'uay'
-                result.push(translation)
-                next
-              end
-                break
-          else
-
-            if char == 'q' and word[i+1] == 'u'
-              qu = true
-              translation = words[i][count + 2..words[i].length] + 'quay'
-              result.push(translation)
-              next
-            else
-              translation += char
-            end
-            count += 1
-          end
-        end
-        if not qu
-          translation = words[i][count..words[i].length] + translation + 'ay'
-          result.push(translation)
-        end
-      end
+    else
+      word = word.slice(1..-1) + word.slice(0)
     end
-    result.join(' ')
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
 end
